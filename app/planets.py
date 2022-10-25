d8329638a0b72bc55c8129890762d73c73cb78f8
Nashwa <n.alsharki@gmail.com>
2022-10-25 10:06:18 -0400

    Refactored get_one_planet code

--- app/planets.py
+++ app/planets.py
@@ -9,74 +9,70 @@ class Planet:
         self.description = description
         self.mass = mass
 
-
 planets = [
     {
         'id': 1,
         'name': 'Mercury',
-                'description': 'Smallest planet',
-                'mass': '3.301×1023 kg'
+        'description': 'Smallest planet',
+        'mass': '3.301×1023 kg'
     },
     {
         'id': 2,
         'name': 'Venus',
-                'description': 'Second planet from the sun',
-                'mass': '4.867×10^24 kg'
+        'description': 'Second planet from the sun',
+        'mass': '4.867×10^24 kg'
     },
     {
         'id': 3,
         'name': 'Earth',
-                'description': 'Our home',
-                'mass': '5.972×10^24 kg'
+        'description': 'Our home',
+        'mass': '5.972×10^24 kg'
     },
     {
         'id': 4,
         'name': 'Mars',
-                'description': 'Some want to go there',
-                'mass': '6.417×10^23 kg'
+        'description': 'Some want to go there',
+        'mass': '6.417×10^23 kg'
     },
     {
         'id': 5,
         'name': 'Jupiter',
-                'description': 'Biggest planet',
-                'mass': '1.899×10^27 kg'
+        'description': 'Biggest planet',
+        'mass': '1.899×10^27 kg'
     },
     {
         'id': 6,
         'name': 'Saturn',
-                'description': 'Has nice rings',
-                'mass': '5.685×10^26 kg'
+        'description': 'Has nice rings',
+        'mass': '5.685×10^26 kg'
     },
     {
         'id': 7,
         'name': 'Uranus',
-                'description': 'Interesting name',
-                'mass': '8.682×10^25 kg'
+        'description': 'Interesting name',
+        'mass': '8.682×10^25 kg'
     },
     {
         'id': 8,
         'name': 'Neptune',
-                'description': 'Last planet',
-                'mass': '1.024×10^26 kg'
+        'description': 'Last planet',
+        'mass': '1.024×10^26 kg'
     }
 ]
 
-planet_list = []
-for p in planets:
-    instant = Planet(p['id'], p['name'], p['description'], p['mass'])
-    planet_list.append(instant)
+planet_list = [Planet(p['id'], p['name'], p['description'], p['mass']) for p in planets]
 
 planets_bp = Blueprint('planets_bp', __name__, url_prefix='/planets')
 
-
 @planets_bp.route('', methods=['GET'])
 def get_all_planets():
-    #planets_reponses =[vars(planet)for planet in planet_list]
     return jsonify(planets)
-
 
 @planets_bp.route('/<id>', methods=['GET'])
 def get_one_planet(id):
-    for planet in planet_list:
-        if planet.id == int(id):
-            return vars(planet)
+    # for planet in planet_list:
+    #     if planet.id == int(id):
+    #         return vars(planet)
+    planet = next((planet for planet in planet_list if planet.id == int(id)), None)
+                  
+    return vars(planet)

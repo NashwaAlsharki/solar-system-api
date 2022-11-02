74f8b54eece34b74da3967b55b3947e60e3a72b8
Nashwa <n.alsharki@gmail.com>
2022-11-02 13:17:15 -0400

    Added query param to get all planets

--- app/routes.py
+++ app/routes.py
@@ -6,24 +6,34 @@ from flask import Blueprint, jsonify, make_response, request, abort
 planets_bp = Blueprint("planets", __name__, url_prefix='/planets')
 
 
-@planets_bp.route('', methods=['GET', 'POST'])
+@planets_bp.route('', methods=['GET'])
 def get_all_planets():
-    if request.method == "GET":
+    name_query = request.args.get('name')
+    if name_query:
+        planets = Planet.query.filter_by(name=name_query)
+    else:
         planets = Planet.query.all()
-        planet_response = []
-        for planet in planets:
-            planet_response.append({"id": planet.id, "name": planet.name,
-                                    "description": planet.description, "mass": planet.mass})
-        return jsonify(planet_response)
-    elif request.method == 'POST':
-        request_body = request.get_json()
-        new_planet = Planet(
-            name=request_body['name'], description=request_body['description'], mass=request_body['mass'])
 
-        db.session.add(new_planet)
-        db.session.commit()
+    planet_response = []
+    for planet in planets:
+        planet_response.append({
+                "id": planet.id, 
+                "name": planet.name,
+                "description": planet.description, 
+                "mass": planet.mass
+        })
+    return jsonify(planet_response)
 
-        return make_response(f"Planet {new_planet.name} successfully created", 201)
+@planets_bp.route('', methods=['POST'])
+def add_planet():
+    request_body = request.get_json()
+    new_planet = Planet(
+        name=request_body['name'], description=request_body['description'], mass=request_body['mass'])
+
+    db.session.add(new_planet)
+    db.session.commit()
+
+    return make_response(f"Planet {new_planet.name} successfully created", 201)
 
 
 def validate_planet(id):
@@ -31,6 +41,7 @@ def validate_planet(id):
         id = int(id)
     except:
         abort(make_response({'message': f'{id} is invalid'}, 400))
+    
     planet = Planet.query.get(id)
     if not planet:
         abort(make_response({"message": f"planet {id} not found"}, 404))
